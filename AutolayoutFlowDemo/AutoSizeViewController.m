28b9beeabf3b0bf9306a5d011ab0cf150ff986b6
Nemo Wang <wangjienemo@gmail.com>
2015-03-06 18:32:53 +0800

    no message

--- AutolayoutFlowDemo/AutoSizeViewController.m
+++ AutolayoutFlowDemo/AutoSizeViewController.m
@@ -8,7 +8,7 @@
 
 #import "AutoSizeViewController.h"
 
-@interface AutoSizeViewController ()<UITextViewDelegate>
+@interface AutoSizeViewController ()
 @property (strong, nonatomic) IBOutlet UIView *layoutView;
 
 @end
@@ -20,8 +20,7 @@
     // Do any additional setup after loading the view.
     self.firstLabel.text = @"   YES,calulate the size use these text.";
     self.firstLabel.preferredMaxLayoutWidth = CGRectGetWidth([UIScreen mainScreen].bounds) -100;
-    self.secondLabel.preferredMaxLayoutWidth = self.firstLabel.preferredMaxLayoutWidth;\
-    self.inputTextView.delegate = self;
+    self.secondLabel.preferredMaxLayoutWidth = self.firstLabel.preferredMaxLayoutWidth;//label的这个属性可以告诉约束系统计算label的size时使用的最大宽度。IB里面也可以设置
     self.inputTextView.returnKeyType  = UIReturnKeyDone;
 }
 
@@ -32,23 +31,29 @@
 
 -(void)viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
-   
-
     [self.view layoutIfNeeded];
 }
 -(void)viewDidLayoutSubviews{
     [super viewDidLayoutSubviews];
     CGSize size = [self.layoutView intrinsicContentSize];
-    CGSize autoSize = [self.layoutView  systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+    CGSize autoSize = [self.layoutView  systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];//这个方法是用约束系统来计算view现在的size
     self.secondLabel.text = [NSString stringWithFormat:@"intrinsicSize = %@,LayoutSize = %@",NSStringFromCGSize(size),NSStringFromCGSize(autoSize)];
-    [self.view invalidateIntrinsicContentSize];
+    [self.view invalidateIntrinsicContentSize];//这个方法会调用 view及其subviews的intrinsicContentSize来计算现在正确的size 来设置view的frame
+    
+    //所以改变view的size有两个方法，一个是重载view的 - intrinsicContentSize方法，并通过调用-invalidateIntrinsicContentSize 方法来改变view的size。另一个是通过自动布局系统调用-systemLayoutSizeFittingSize 来重新布局
 
 }
 
 
 - (IBAction)animationViewSize:(id)sender {
+    
     [self.inputTextView resignFirstResponder];
     self.firstLabel.text = self.inputTextView.text;
-    [self.view invalidateIntrinsicContentSize];
+    [UIView animateWithDuration:0.4 animations:^{
+        [self.view setNeedsLayout];
+        [self.view layoutIfNeeded];
+    }];
+   
+    
 }
 @end
